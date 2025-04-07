5baaadf265013f8c5c61b6a3bda566e30c0ebe1d
Josiah McCorkle <josiahmccorkle@MacBookPro.localdomain>
2025-04-07 14:49:13 -0400

    send an email of feed

--- RSS_pinger.py
+++ RSS_pinger.py
@@ -2,6 +2,9 @@ import time
 import requests
 import feedparser
 import rss_constants
+import secrets_file
+import smtplib
+from email.mime.text import MIMEText
 
 def fetch_rss_feed(url):
     try:
@@ -9,9 +12,28 @@ def fetch_rss_feed(url):
         response.raise_for_status()
         feed = feedparser.parse(response.text)
         print(f"Fetched {len(feed.entries)} entries from {url}")
+        emailBody = ''
+
         for entry in feed.entries:
             print(f"Title: {entry.title}")
             print(f"Link: {entry.link}\n")
+            emailBody += f"{entry.title}\n{entry.link}\n\n"
+
+        try:
+            msg = MIMEText(emailBody)
+            msg["Subject"] = "Mozilla Positions"
+            msg["From"] = secrets_file.EMAIL_ADDRESS
+            msg["To"] = secrets_file.EMAIL_ADDRESS
+
+            with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+                # server.set_debuglevel(1)
+                print("logging in to email!")
+                server.login(secrets_file.EMAIL_ADDRESS, secrets_file.GMAIL_PASSWORD)
+                print("sending email!")
+                server.send_message(msg)
+                print("Email sent successfully!")
+        except Exception as e:
+            print(f"Failed to send email: {e}")
     except requests.RequestException as e:
         print(f"Error fetching RSS feed: {e}")
 
--- rss_constants.py
+++ rss_constants.py
@@ -1,2 +1,2 @@
 CAREERS_FEED = "https://www.mozilla.org/en-US/careers/feed/"
-DELAY_INTERVAL = 30
+DELAY_INTERVAL = 3600
