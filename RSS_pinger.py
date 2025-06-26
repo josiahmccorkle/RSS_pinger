52c5497579f9d9ce67b5e7b373e40274f05149b0
Josiah McCorkle <josiahmccorkle@gmail.com>
2025-06-25 21:10:33 -0400

    update constants and gitignore

--- RSS_pinger.py
+++ RSS_pinger.py
@@ -15,12 +15,10 @@ def fetch_rss_feed(url):
         emailBody = ''
 
         for entry in feed.entries:
-            # print(f"Title: {entry.title}")
-            # print(f"Link: {entry.link}\n")
             emailBody += f"{entry.title}\n{entry.link}\n\n"
 
             msg = MIMEText(emailBody)
-            msg["Subject"] = "Mozilla Positions"
+            msg["Subject"] = secrets_file.EMAIL_SUBJECT
             msg["From"] = secrets_file.EMAIL_ADDRESS
             msg["To"] = secrets_file.EMAIL_ADDRESS
 
@@ -33,3 +31,14 @@ RSS_FEED_URL = rss_constants.CAREERS_FEED
 while True:
     fetch_rss_feed(RSS_FEED_URL)
     time.sleep(rss_constants.DELAY_INTERVAL)
+
+    # timeToEmail = [7, 11, 3, 19]
+    # local_time = time.localtime().tm_hour
+    # fetch_rss_feed(RSS_FEED_URL)
+    # print(time.localtime().tm_hour, time.localtime().tm_min)
+
+
+
+    # if local_time in timeToEmail and time.localtime().tm_min == 0:
+    #     time.sleep(rss_constants.DELAY_INTERVAL)
+    #     fetch_rss_feed(RSS_FEED_URL)
